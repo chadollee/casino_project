43b00b54f7176767f7e70e435c031e1afef5fd0d
chadol lee <chadollee@protonmail.com>
2020-05-27 20:25:08 -0700

    final version

--- casino.rb
+++ casino.rb
@@ -3,92 +3,116 @@
 # Slots
 # High / Low
 # Player places bet and wins / loses (hint: rand)
-# Player's bankroll goes up and down with wins and losses
-
-
-require_relative 'deck'
-
-# class Player
-#   # able to read and write the name and age
-#   attr_accessor :name, :bankroll
-#   # attr_reader only read
-#   # attr_writer only write
-#   def initialize(name, bankroll)
-#     @name = name
-#     @bankroll = bankroll
-#   end
-
-
-
-@name = []
-@bankroll = []
-
-
-def high_low
-  puts "high_low game"
-end
-
-
-
-def pull_lever
-  rand_num = rand(3)
-  case rand_num
-  when 1
-    puts "lemon, grape, banana"
-    puts "you lose!"
-  when 2
-    puts "cherry, lemon, cherry"
-    puts "You won something"
-  when 3
-    puts "cherry, cherry, cherry"
-    puts "You win big!"
-    
-  else 
-    puts "try again"
+# Player’s bankroll goes up and down with wins and losses
+class Person
+  attr_accessor :name, :bankaccount
+  def initialize(name, bankaccount)   #initial method
+    @name = name
+    @bankaccount = bankaccount
+  end
+  def update_account
+    @bankaccount = @bankaccount + @change
   end
 end
-
-
-def slots
-  puts "How much money would you like to play?"
-  cash = gets.chomp.to_i
-
-  puts "Total cash: $#{cash}"
-
-  puts "Pull lever"
-  pull_lever
-
+@cash = []
+@you_lose = 0
+@you_won_something = 200
+@you_won_big = 500
+def pull_lever
+  rand_num = rand(4)
+  case rand_num
+  when 1
+    puts “lemon, grape, banana”
+    @change = 0
+    puts “you lose $#{@you_lose}”
+    @bankaccount = @bankaccount - @you_lose
+    puts “Your bank value is now #{@bankaccount}”
+  when 2
+    puts “cherry, lemon, cherry”
+    puts “You won $#{@you_won_something}!”
+    @bankaccount = @bankaccount + @you_won_something
+    puts “Your bank value is now #{@bankaccount}”
+  when 3
+    puts  “grape, lemon, cherry”
+    puts “You won $#{@you_won_something}!”
+    @bankaccount = @bankaccount + @you_won_something
+    puts “Your bank value is now #{@bankaccount}”
+  when 4
+    puts “cherry, cherry, cherry”
+    puts “You win big $#{@you_won_big}!”
+    @bankaccount = @bankaccount + @you_won_big
+    puts “Your bank value is now #{@bankaccount}”
+  else
+    puts “lemon, lemon, lemon”
+    @change = 0
+    puts “you lose $#{@you_lose}”
+    @bankaccount = @bankaccount - @you_lose
+    puts “Your bank value is now #{@bankaccount}”
+  end
+  game_menu
 end
-
-
-
 def intro
-  puts 'Welcome to DevPoint Labs Casino!'
-  puts 'Hello, what is your name?'
+  puts ‘Welcome to DevPoint Labs Casino!’
+  puts “What is your name?”
   @name = gets.strip
-  puts 'How much do you want to wager?'
-  @bankroll = gets.to_i
-  puts "$#{@bankroll} available: Welcome, #{@name.capitalize!}, lets get started!"
+  puts “How much you got in that bank account?”
+  @bankaccount = gets.to_i
+  puts “$#{@bankaccount} available: Welcome, #{@name.capitalize!}, lets get started!”
 end
-
 intro
-
+def high_low
+  number = rand(1..10)
+  puts “How much money would you like to wager?”
+  @cash = gets.to_i
+  puts “Guess the correct number between 1 and 10:”
+    user_input = gets.chomp.to_i
+    if user_input == number
+      puts “You picked the correct number!”
+      @change = @cash
+      @bankaccount = @bankaccount + @cash
+      puts “Your bank value is now #{@bankaccount}”
+    elsif user_input > number
+      puts “The correct number is #{number} ... you lost your bet!”
+      @bankaccount = @bankaccount - @cash
+      puts “Your bank value is now #{@bankaccount}”
+    else
+      puts “The correct number is #{number} ... you lost your bet!”
+      @bankaccount = @bankaccount - @cash
+      puts “Your bank value is now #{@bankaccount}”
+    end
+  game_menu
+  end
+def slots
+  puts “How much money would you like to wager?”
+  @cash = gets.chomp.to_i
+  puts “Total cash: $#{@cash}”
+  puts “Do you want to spin the lever?”
+  puts “1). Yes”
+  puts “2). No”
+  choice = gets.to_i
+  case choice
+  when 1
+  pull_lever
+  when 2
+  intro
+  else
+  puts “invalid choice try again”
+  end
+end
 def game_menu
-  puts 'What would you like to play?'
-  puts '1) Slots'
-  puts '2) High/ Low'
-  
+  puts ‘What would you like to play?’
+  puts ‘1) Slots’
+  puts ‘2) High/ Low’
+  puts ‘3) Ive had enough!’
   user_input = gets.strip.to_i
   if user_input === 1
     slots
   elsif user_input === 2
     high_low
-  else 
+  elsif user_input === 3
+    puts “thanks for playing!”
+  else
     intro
   end
-  
 end
-
-
-
 game_menu
